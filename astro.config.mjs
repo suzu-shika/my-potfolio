49c784923e2a3c4c2f11fd95d5adb71d22b63142
suzu-shika <msz.006.ts@gmail.com>
2024-11-08 09:03:14 +0900

    robots.txtを設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,8 @@
 import { defineConfig } from 'astro/config';
-import robotsTxt from 'astro-robots-txt';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://portfolio.suzu-shika.com/', // サイトのURL
-  integrations: [
-    sitemap(),
-    robotsTxt({
-      policy: [
-        {
-          userAgent: '*',
-          allow: '/',
-        },
-      ],
-      sitemap: 'https://portfolio.suzu-shika.com/sitemap.xml', // サイトマップのURL
-    }),
-  ],
+  site: 'https://portfolio.suzu-shika.com',
+  integrations: [sitemap()],
 });
